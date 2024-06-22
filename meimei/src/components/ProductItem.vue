96d94aa55f4f2717754c4a8005656ef722405d42
anja <anjamestrovic23@gmail.com>
2024-06-22 20:54:45 +0200

    ProductItem,List,Menu,Home

--- meimei/src/components/ProductItem.vue
+++ meimei/src/components/ProductItem.vue
@@ -1,41 +1,49 @@
 <template>
-    <div class="product-item">
-      <img :src="product.image" :alt="product.name" />
-      <h3>{{ product.name }}</h3>
-      <p>{{ product.description }}</p>
-      <p>€{{ product.price.toFixed(2) }}</p>
-      <button @click="addToCart">Dodaj</button>
-    </div>
-  </template>
+<div class="okolo">
+  <div v-if="dish" class="dish-details">
+      <img :src="dish.image" :alt="dish.name" />
+      <h3>{{ dish.name }}</h3>
+      <p>{{ dish.description }}</p>
+      <input type="radio" name="size" value="small" class="radio">
+      <p>Small: €{{ dish.priceSmall.toFixed(2) }}</p>
+      <input type="radio" name="size" value="large" class="radio">
+      <p>Large: €{{ dish.priceLarge.toFixed(2) }}</p>
+      <button @click="addToCart">Add to cart</button>
+      <button @click="$emit('close')">Back to menu</button>
+  </div>
+
+    
+</div>
+    
+</template>
   
-  <script>
+<script>
   export default {
-    name: 'ProductItem',
     props: {
-      product: Object
+      dish: Object
     },
     methods: {
-      addToCart() {
-        this.$emit('add-to-cart', this.product)
+      addToCart(){
+
       }
     }
+};
+</script>
+  
+<style scoped>
+  .okolo{
+    justify-content: center;
+    align-content: center;
+    align-items: center;
   }
-  </script>
-  
-  <style scoped>
-  
-  .product-item {
-    
+  .dish-details {
     padding: 20px;
     text-align: center;
     background-color: rgb(249, 213, 219);
-    width: 350px;
-    height: 400px;
     font-family: "Roboto Mono", monospace;
-    border: 1px solid red;
   }
   
-  .product-item img {
+  .dish-details img {
     width: 150px;
     height: auto;
   }
@@ -44,7 +52,6 @@
     padding-top: 20px;
     color: red;
   }
-  
   button {
     background-color: red;
     color: white;
@@ -58,5 +65,20 @@
   button:hover {
     background-color: #ff4d4d;
   }
-  </style>
+  input[type="radio"]{
+    border-radius: 50%;
+    width: 20px;
+    height: 20px;
+    border: 2px solid #ff4d4d;
+    accent-color: rgb(249, 213, 219);
+    outline: none;
+    cursor: pointer;
+  }
+  input[type="radio"]:checked{
+    accent-color: #ff4d4d;
+
+  }
+  
+  
+</style>
   
--- meimei/src/views/Menu.vue
+++ meimei/src/views/Menu.vue
@@ -1,24 +1,130 @@
 <template>
     <div class="menu">
       <h1>Meni</h1>
-      <!--<DishList group="Предјела" />
-      <DishList group="Главна јела" />
-      <DishList group="Дезерт" />-->
+      <div>
+        <button @click="currentGroup = 'predjelo';selectedDish=null;">Appetizers</button>
+        <button @click="currentGroup = 'glavna';selectedDish=null;">Main Courses</button>
+        <button @click="currentGroup = 'dezerti';selectedDish=null;">Desserts</button>
+        <button @click="currentGroup = 'kokteli';selectedDish=null;">Cocktails</button>
+        <button @click="currentGroup = 'boba';selectedDish=null;">Bubble tea</button>
+      </div>
+      <ProductList v-if="currentGroup" :dishes="groups[currentGroup]" @show-details="showddetails" />
+      <ProductItem v-if="selectedDish" :dish="selectedDish" @close="selectedDish = null; currentGroup='predjelo'" />
     </div>
-  </template>
+</template>
   
   <script>
- 
-  
+ import ProductList from '../components/ProductList.vue'
+ import ProductItem from '../components/ProductItem.vue'
+
   export default {
     name: 'Menu_view',
-    
-  }
+    components:{
+      ProductList,
+      ProductItem
+    },
+    data() {
+      return {
+        currentGroup: 'predjelo',
+        selectedDish: null,
+        groups: {
+          predjelo: [
+            {
+              id: 1, 
+              name: 'Spring rolls',
+              priceSmall: 6.80, 
+              priceLarge: 8.80,
+              description: 'Crispy, golden-brown spring rolls packed with a mix of shredded cabbage, carrots, and glass noodles, wrapped in a delicate rice paper, served with a side of our signature sweet chili dipping sauce', 
+              image: '/Hrana/springrolls.PNG' 
+            },
+          ],
+          glavna: [
+            { 
+                id: 2, 
+                name: 'Bao Buns',
+                priceSmall: 6.80, 
+                priceLarge: 8.80,
+                description: 'Soft, fluffy, steamed buns filled with tender, marinated pork belly paired with fresh, crunchy cucumbers and a hint of hoisin sauce.', 
+                image: '/Hrana/baobuns.PNG' 
+            },
+            { 
+                id: 3, 
+                name: 'BBQ Pork',
+                priceSmall: 6.80, 
+                priceLarge: 8.80,
+                description: 'Tender slices of Chinese barbecue pork (Char Siu), marinated and glazed with a savory-sweet sauce. Served over a bed of steamed jasmine rice, accompanied by fresh seasonal vegetables and a drizzle of our special house sauce.', 
+                image: '/Hrana/bbqpork.PNG' 
+            },
+            { 
+                id: 4, 
+                name: 'Noodles',
+                priceSmall: 0.80, 
+                priceLarge: 9.80,
+                description: 'Stir-fried noodles tossed with fresh green onions and rich, flavorful duck, expertly roasted to tender perfection.', 
+                image: '/Hrana/noodles.PNG' 
+            },
+          ],
+          dezerti: [
+            { 
+              id: 5, 
+              name: 'Toffee Banana',
+              priceSmall: 6.80, 
+              priceLarge: 8.80,
+              description: 'Sweet, caramelized bananas drizzled with a rich toffee sauce, served warm and paired with a scoop of creamy vanilla ice cream.', 
+              image: '/Hrana/toffeebanana.PNG' 
+            },
+          ],
+          kokteli:[
+            {
+              id: 6,
+              name: 'Meigroni',
+              priceSmall: 12.00,
+              priceLarge: 15.80,
+              description: 'Our signature cocktail',
+              image: '/Hrana/IMG_4444.PNG'
+            }
+          ],
+          boba:[
+            {
+              id: 7,
+              name: 'Matcha boba',
+              priceSmall: 7.00,
+              priceLarge: 9.20,
+              description: 'Boba tea with matcha and milk',
+              image: '/Hrana/IMG_4448.PNG'
+            }
+          ]
+        }
+      };
+    },
+    methods: {
+      showddetails(dish) {
+        this.selectedDish = dish;
+        this.currentGroup=null;
+      }
+    }
+  };
+
+  
+  
   </script>
   
   <style scoped>
   .menu {
     padding: 20px;
+  }
+  button {
+    background-color: red;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 20px;
+    cursor: pointer;
+    font-style: bold;
+  }
+  
+  button:hover {
+    background-color: #ff4d4d;
   }
   </style>
   
