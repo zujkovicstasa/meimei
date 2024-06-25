822b82fbc0f24e875e120584727a2bbd698dba69
zujkovicstasa <zujkovicstasa@gmail.com>
2024-06-25 16:51:51 +0200

    .

---
+++ meimei/src/locale/en.js
@@ -0,0 +1,130 @@
+const messages = {
+  
+    menu: {
+      predjela: [
+        {
+          id: 1, 
+          name: 'Spring rolls',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Crispy, golden-brown spring rolls packed with a mix of shredded cabbage, carrots, and glass noodles, wrapped in a delicate rice paper, served with a side of our signature sweet chili dipping sauce', 
+          image: '/Hrana/springrolls.PNG'
+        },
+      ],
+      glavna: [
+        { 
+          id: 2, 
+          name: 'Bao Buns',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Soft, fluffy, steamed buns filled with tender, marinated pork belly paired with fresh, crunchy cucumbers and a hint of hoisin sauce.', 
+          image: '/Hrana/baobuns.PNG' 
+        },
+        { 
+          id: 3, 
+          name: 'BBQ Pork',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Tender slices of Chinese barbecue pork (Char Siu), marinated and glazed with a savory-sweet sauce. Served over a bed of steamed jasmine rice, accompanied by fresh seasonal vegetables and a drizzle of our special house sauce.', 
+          image: '/Hrana/bbqpork.PNG' 
+        },
+        { 
+          id: 4, 
+          name: 'Noodles',
+          priceSmall: 0.80, 
+          priceLarge: 9.80,
+          description: 'Stir-fried noodles tossed with fresh green onions and rich, flavorful duck, expertly roasted to tender perfection.', 
+          image: '/Hrana/noodles.PNG' 
+        },
+      ],
+      dezerti: [
+        { 
+          id: 5, 
+          name: 'Toffee Banana',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Sweet, caramelized bananas drizzled with a rich toffee sauce, served warm and paired with a scoop of creamy vanilla ice cream.', 
+          image: '/Hrana/toffeebanana.PNG' 
+        },
+        { 
+          id: 6, 
+          name: 'Fried Ice Cream',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Vanilla ice cream delicately coated in a crunchy breadcrumb crust, drizzled with warm caramel sauce and crowned with a dollop of whipped cream.', 
+          image: '/Hrana/friedicecream.PNG' 
+        },
+        { 
+          id: 7, 
+          name: 'Chocolate Ice Cream',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Dark chocolate ice cream infused with pieces of dark orange chocolate throughout, served with a slice of fresh orange for a refreshing citrus finish.', 
+          image: '/Hrana/icecream.PNG' 
+        },
+      ],
+      kokteli:[
+        {
+          id: 8,
+          name: 'Meigroni',
+          priceSmall: 12.00,
+          priceLarge: 15.80,
+          description: 'Our signature cocktail crafted with equal parts gin, sweet vermouth, and Campari, garnished with an orange twist for a bittersweet and sophisticated sip.',
+          image: '/Hrana/IMG_4444.PNG'
+        },
+        { 
+          id: 9, 
+          name: 'Aperol Spritz',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'A refreshing blend of Aperol, prosecco, and soda water, garnished with a slice of orange for a zesty twist.', 
+          image: '/Hrana/IMG_4445.PNG' 
+        },
+        { 
+          id: 10, 
+          name: 'Cosmopolitan',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'A classic cocktail featuring vodka, triple sec, cranberry juice, and a splash of fresh lime juice, shaken and strained into a chilled martini glass with a twist of orange peel.', 
+          image: '/Hrana/IMG_4447.PNG' 
+        },
+        { 
+          id: 11, 
+          name: 'Mojito',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'A refreshing Cuban cocktail made with white rum, fresh mint leaves, lime juice, sugar, and soda water, served over ice for a cool and invigorating drink.', 
+          image: '/Hrana/IMG_4449.PNG' 
+        },
+      ],
+      boba:[
+        { 
+          id: 12, 
+          name: 'Ice caramel boba latte',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Our signature blend of rich caramel syrup, chilled milk, and espresso, topped with chewy tapioca pearls for a delightful twist on a classic latte experience.', 
+          image: '/Hrana/IMG_4446.PNG' 
+        },
+        {
+          id: 13,
+          name: 'Matcha boba',
+          priceSmall: 7.00,
+          priceLarge: 9.20,
+          description: 'A modern twist on traditional matcha tea infused with creamy milk and served with chewy tapioca pearls, offering a delightful combination of earthy matcha flavor and satisfying texture.',
+          image: '/Hrana/IMG_4448.PNG'
+        },
+        {
+          id: 14,
+          name: 'Strawberry milkshake boba',
+          priceSmall: 7.00,
+          priceLarge: 9.20,
+          description: 'A delightful fusion of creamy milkshake and refreshing bubble tea, featuring sweet strawberry flavor blended with milk and topped with chewy tapioca pearls for a unique and satisfying treat.',
+          image: '/Hrana/IMG_4450.PNG'
+        }
+      ]
+    }
+};
+
+
+export default messages;
---
+++ meimei/src/locale/sr.js
@@ -0,0 +1,130 @@
+const messages = {
+  
+    menu: {
+      predjela: [
+        {
+          id: 1, 
+          name: 'Prolećne rolnice',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Hrskave, zlatne rolnice punjene mešavinom rendanog kupusa, šargarepe i staklenih rezanaca, umotane u pirinčanu papir, poslužene uz naš prepoznatljiv sos od slatkog čilija.', 
+          image: '/Hrana/springrolls.PNG'
+        },
+      ],
+      glavna: [
+        { 
+          id: 2, 
+          name: 'Bao Buns',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Meka, vazdušasta, peciva spremana na pari, napunjena mekanim, mariniranim svinjskim mesom uparenim sa svežim, hrskavim krastavcima i hoisin sosom.', 
+          image: '/Hrana/baobuns.PNG' 
+        },
+        { 
+          id: 3, 
+          name: 'BBQ Svinjetina',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Mekani komadi kineske roštiljske svinjetine (Char Siu), marinirani i glazirani sa slatko-slanim sosom. Posluženi preko pirinča od jasmina spremanog na pari, uz sveže sezonsko povrće sa našim domaćim sosom.', 
+          image: '/Hrana/bbqpork.PNG' 
+        },
+        { 
+          id: 4, 
+          name: 'Rezanci',
+          priceSmall: 0.80, 
+          priceLarge: 9.80,
+          description: 'Prženi rezanci pomešani sa svežim mladim lukom i bogatom, aromatičnom patkom, pečenom do savršenstva.', 
+          image: '/Hrana/noodles.PNG' 
+        },
+      ],
+      dezerti: [
+        { 
+          id: 5, 
+          name: 'Tofi Banana',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Slatke, karamelizovane banane prelivene bogatim sosom od karamele, poslužene tople uz kuglu kremastog sladoleda od vanile.', 
+          image: '/Hrana/toffeebanana.PNG' 
+        },
+        { 
+          id: 6, 
+          name: 'Pohovani Sladoled',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Sladoled od vanile obložen hrskavim prezlama, preliven toplim karamel sosom i ukrašen sa šlagom.', 
+          image: '/Hrana/friedicecream.PNG' 
+        },
+        { 
+          id: 7, 
+          name: 'Čokoladni Sladoled',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Tamni čokoladni sladoled prožet komadima tamne čokolade sa narandžom, poslužen sa kriškom sveže narandže za osvežavajući citrusni spoj.', 
+          image: '/Hrana/icecream.PNG' 
+        },
+      ],
+      kokteli:[
+        {
+          id: 8,
+          name: 'Meigroni',
+          priceSmall: 12.00,
+          priceLarge: 15.80,
+          description: 'Naš prepoznatljiv koktel napravljen sa jednakim delovima džina, slatkog vermuta i Camparija, ukrašen spiralom narandže za gorak i sofisticiran gutljaj.',
+          image: '/Hrana/IMG_4444.PNG'
+        },
+        { 
+          id: 9, 
+          name: 'Aperol Spritz',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Osvežavajuća mešavina Aperola, proseka i soda vode, ukrašena kriškom narandže.', 
+          image: '/Hrana/IMG_4445.PNG' 
+        },
+        { 
+          id: 10, 
+          name: 'Cosmopolitan',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Klasičan koktel sa vodkom, triple sec, sokom od brusnice i kapljicom svežeg soka od limete, protresen i procijeđen u ohlađenu čašu za martini sa spiralom narandže.', 
+          image: '/Hrana/IMG_4447.PNG' 
+        },
+        { 
+          id: 11, 
+          name: 'Mojito',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Osvežavajući kubanski koktel napravljen sa belim rumom, svežim listovima nane, sokom od limete, šećerom i soda vodom, serviran preko leda za osvežavajući ugodjaj.', 
+          image: '/Hrana/IMG_4449.PNG' 
+        },
+      ],
+      boba:[
+        { 
+          id: 12, 
+          name: 'Ledeni karamel boba latte',
+          priceSmall: 6.80, 
+          priceLarge: 8.80,
+          description: 'Naša prepoznatljiva mešavina bogatog karamel sirupa, ohlađenog mleka i espresa, prelivena sa žvakaćim tapioka perlama za prijatan zaokret na klasičnom latte iskustvu.', 
+          image: '/Hrana/IMG_4446.PNG' 
+        },
+        {
+          id: 13,
+          name: 'Matcha boba',
+          priceSmall: 7.00,
+          priceLarge: 9.20,
+          description: 'Moderan zaokret na tradicionalni matcha čaj infuziran sa kremastim mlekom i serviran sa žvakaćim tapioka perlama, nudi prijatnu kombinaciju zemljanog matcha ukusa i zadovoljavajuće teksture.',
+          image: '/Hrana/IMG_4448.PNG'
+        },
+        {
+          id: 14,
+          name: 'Boba milkšejk od jagode',
+          priceSmall: 7.00,
+          priceLarge: 9.20,
+          description: 'Prijatan spoj kremastog milkšejka i osvežavajućeg bubble tea, sa slatkim ukusom jagode pomešanim sa mlekom i prelivenim žvakaćim tapioka perlama za jedinstven i zadovoljavajući užitak.',
+          image: '/Hrana/IMG_4450.PNG'
+        }
+      ]
+    
+  }
+};
+
+export default messages;
