1ec01094ec988013a19ae38ff1361c008325fcc4
anja <anjamestrovic23@gmail.com>
2024-06-26 02:10:28 +0200

    footer

--- meimei/src/components/Footer.vue
+++ meimei/src/components/Footer.vue
@@ -1,8 +1,20 @@
 <template>
+<div>
+  <div class="banner"> 
+        <a href="https://pinterest.com" class="banner-link"><img src="/Hrana/pinterest.png" alt="" class="lg" target="_blank"></a>
+        <a href="https://google.com" class="banner-link"><img src="/Hrana/google.png" alt="" class="lg" target="_blank"></a>
+        <a href="https://instagram.com" class="banner-link"><img src="/Hrana/instagram.png" alt="" class="lg" target="_blank"></a>
+    
+      </div>
     <footer class="site-footer">
-      <p>Copyright 2024, Anja Meštrović i Staša Žujković, Odsek za softversko inženjerstvo Elektrotehničkog fakulteta u Beogradu</p>
+      
+       
+      <div>
+        <p>Copyright 2024, Anja Meštrović i Staša Žujković, Odsek za softversko inženjerstvo Elektrotehničkog fakulteta u Beogradu</p>
+      </div>
     </footer>
-  </template>
+    </div>
+</template>
   
   <script>
   export default {
@@ -21,10 +33,38 @@
     background-color: rgb(249, 213, 219);
     border-top: 2px solid red;
     width: 100%;
+    
   }
   .site-footer p{
     font-size:15px;
     color:red;
   }
+  .banner {
+  margin-top: 30px;
+  display: flex;
+  align-content: center;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+    flex-wrap: wrap;
+    align-content: space-between;
+    justify-content: center;
+    margin-bottom: 10px;
+}
+
+.banner-link {
+  margin: 0 10px;
+  color: #007bff;
+  text-decoration: none;
+}
+
+.banner-link:hover {
+  text-decoration: underline;
+}
+.lg{
+  width:50px;
+  height: 50px;
+  width: auto;
+}
   </style>
   
